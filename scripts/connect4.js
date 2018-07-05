ac0f978f142ef76e61f8883a024e9ac206135ad5
John Kennedy <nextechcatapult@arms84.localdomain>
2018-07-05 15:34:04 -0400

    lal

--- scripts/connect4.js
+++ scripts/connect4.js
@@ -9,17 +9,21 @@ var column = document.querySelectorAll(".column");
 Connect4 = function(event){
     console.log("click")
     let cell = event.target;
-    cell.innerHTML = currentPlayer;
+    //cell.innerHTML = currentPlayer;
     if (currentPlayer==="red"){
+        /*
         let checker = document.createElement("div");
         checker.classList.add("checker","red");
-        cell.appendChild(checker);
+        cell.appendChild(checker);;*/
         currentPlayer="black";
+        cell.classList+=' '+currentPlayer;
 }else if(currentPlayer ==="black") {
+    /*
     let checker = document.createElement("div");
         checker.classList.add("checker","black");
-        cell.appendChild(checker);
-        currentPlayer = "red";
+        cell.appendChild(checker);;*/
+        currentPlayer = "red"
+        cell.classList+=' '+currentPlayer;
   }
 }
 
